79287d9d002348cd6775817bf74bab252903d69d
Jai Vadula <jaivadula@gmail.com>
2024-06-03 00:01:55 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 
 from src.components.data_transformation import (DataTransformation,
                                                 DataTransformationConfig)
+from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 from src.exception import CustomException
 from src.logger import logging
 
@@ -55,4 +56,7 @@ if __name__=="__main__":
     train_data,test_data,raw_data = obj.initiate_data_ingestion()
     
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
